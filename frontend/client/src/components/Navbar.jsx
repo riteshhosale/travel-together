50ae4a25734396c85a5d341de0d7b22296640635
ritesh bhosale <riteshbhosale092007@gmail.com>
2026-05-29 19:13:52 +0530

    fix: remove live site buttons

--- frontend/client/src/components/Navbar.jsx
+++ frontend/client/src/components/Navbar.jsx
@@ -26,14 +26,6 @@ function Navbar() {
 
   const authButtons = authed ? (
     <>
-      <a
-        href='https://travel-together-o41z.onrender.com/'
-        target='_blank'
-        rel='noreferrer'
-        className='fg-btn-secondary text-xs'
-      >
-        Live site
-      </a>
       <Link to='/profile' className='fg-btn-secondary text-xs' onClick={() => setMenuOpen(false)}>
         Profile
       </Link>
@@ -46,14 +38,6 @@ function Navbar() {
     </>
   ) : (
     <>
-      <a
-        href='https://travel-together-o41z.onrender.com/'
-        target='_blank'
-        rel='noreferrer'
-        className='fg-btn-secondary text-xs'
-      >
-        Live site
-      </a>
       <Link to='/login' className='fg-btn-secondary text-xs' onClick={() => setMenuOpen(false)}>
         Login
       </Link>
