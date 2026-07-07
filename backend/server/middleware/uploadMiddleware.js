5095f3aa51465c10e6017258a46cfc22be7ad8c3
ritesh bhosale <riteshbhosale092007@gmail.com>
2026-07-07 18:13:32 +0530

    Fix import statement for CloudinaryStorage in uploadMiddleware.js

--- backend/server/middleware/uploadMiddleware.js
+++ backend/server/middleware/uploadMiddleware.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const CloudinaryStorage = require('multer-storage-cloudinary');
 
 const { cloudinary, cloudinaryEnabled } = require('../utils/imageStorage');
 
